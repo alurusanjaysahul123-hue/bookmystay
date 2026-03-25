5a5a2e971d49f46403bd6f34d47ecebd508a33e7
brucemoses01-lab <brucemoses01@gmail.com>
2026-03-25 15:02:32 +0530

    uc1 updated

---
+++ bookmystays/src/HotelBookingApp.java
@@ -0,0 +1,20 @@
+/**
+ * Hotel Booking Application Entry Point
+ * Prints a welcome message to the console.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0");
+        System.out.println("=================================");
+
+        // Application ends here
+    }
+}
